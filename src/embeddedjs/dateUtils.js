6707033eb220124baedf3ee870d96c9032700057
Grzegorz Czernatowicz <gczernatowicz@proton.me>
2026-09-15 20:19:49 +0200

    niedziela fix
    show current niedziela if niedziela instead of next niedziela

--- src/embeddedjs/dateUtils.js
+++ src/embeddedjs/dateUtils.js
@@ -5,6 +5,7 @@ class DateUtils {
   
   getNextSundayDate() {
     const now = new Date();
+    if (now.getDay() === 0) return now;
     now.setDate(now.getDate() + 7 - now.getDay());
     return now;
   }
