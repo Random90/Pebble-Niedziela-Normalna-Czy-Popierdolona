004d9c8679b1f2a80a078747ffef102ba971708a
Grzegorz Czernatowicz <gczernatowicz@proton.me>
2026-09-16 23:59:34 +0200

    detecting normal sundays working
    basic functionality is finished

--- src/embeddedjs/dateUtils.js
+++ src/embeddedjs/dateUtils.js
@@ -1,13 +1,114 @@
 class DateUtils {
-  isNextSundayNormal() {
+  normalMonths = [0, 3, 5, 7];
+  rigidHolidays = [
+    '01.01', // Nowy Rok
+    '01.06', // Trzech Króli
+    '05.01', // Święto Pracy
+    '05.03', // Święto Konstytucji 3 Maja
+    '08.15', // Wniebowzięcie NMP / Święto Wojska Polskiego
+    '11.01', // Wszystkich Świętych
+    '11.11', // Święto Niepodległości
+    '12.24', // Wigilia
+    '12.25', // Boże Narodzenie (Dzień 1)
+    '12.26', // Boże Narodzenie (Dzień 2)
+  ];
+
+  isNextSundayNormal(customDate = null) {
+    const nextSunday = this.getNextSundayDate(customDate);
+    if (this.isHoliday(nextSunday)) return false;
+    if (this.isLastSundayOfNormalMonth(nextSunday)) return true;
+    if (this.isSundayBeforeEaster(nextSunday)) return true;
+    if (this.isSundayBeforeChristmas(nextSunday)) return true;
     return false;
   }
-  
-  getNextSundayDate() {
-    const now = new Date();
+
+  getNextSundayDate(customDate = null) {
+    let now;
+    if (customDate !== null) {
+      now = new Date(customDate);
+    } else {
+      now = new Date();
+    }
+    now.setHours(0, 0, 0, 0);
     if (now.getDay() === 0) return now;
     now.setDate(now.getDate() + 7 - now.getDay());
     return now;
+  }
+
+  isLastSundayOfNormalMonth(sundayDate) {
+    sundayDate = new Date(sundayDate);
+    sundayDate.setHours(0, 0, 0, 0);
+    const currentMonth = sundayDate.getMonth();
+    if (!this.normalMonths.includes(currentMonth)) return false;
+    sundayDate.setDate(sundayDate.getDate() + 7);
+    if (sundayDate.getMonth() !== currentMonth) return true;
+    return false;
+  }
+
+  getEaster(date) {
+    date = new Date(date);
+    const year = date.getFullYear();
+    const a = year % 19;
+    const b = Math.floor(year / 100);
+    const c = year % 100;
+    const d = Math.floor(b / 4);
+    const e = b % 4;
+    const f = Math.floor((b + 8) / 25);
+    const g = Math.floor((b - f + 1) / 3);
+    const h = (19 * a + b - d - g + 15) % 30;
+    const i = Math.floor(c / 4);
+    const k = c % 4;
+    const L = (32 + 2 * e + 2 * i - h - k) % 7;
+    const m = Math.floor((a + 11 * h + 22 * L) / 451);
+    const easterMonth = Math.floor((h + L - 7 * m + 114) / 31) - 1; // convert 1-indexed to 0-indexed
+    const easterDay = ((h + L - 7 * m + 114) % 31) + 1;
+
+    return new Date(Date.UTC(year, easterMonth, easterDay));
+  }
+
+  isHoliday(date) {
+    date = new Date(date);
+    date.setHours(0, 0, 0, 0);
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const currentFormatted = `${month}.${day}`;
+    if (this.rigidHolidays.includes(currentFormatted)) return true;
+    const easter = this.getEaster(date);
+    const corpusChristi = new Date(easter.getTime());
+    corpusChristi.setDate(easter.getDate() + 60);
+    const ccFormatted = `${String(corpusChristi.getMonth() + 1).padStart(2, '0')}.${String(corpusChristi.getDate()).padStart(2, '0')}`;
+    if (currentFormatted === ccFormatted) return true;
+    return false;
+  }
+
+  isSundayBeforeEaster(sundayDate) {
+    sundayDate = new Date(sundayDate);
+    sundayDate.setHours(0, 0, 0, 0);
+    if (![2, 3].includes(sundayDate.getMonth())) return false;
+    const easter = this.getEaster(sundayDate);
+    const sundayBeforeEaster = new Date(easter.getTime());
+    sundayBeforeEaster.setDate(easter.getDate() - 7);
+    sundayBeforeEaster.setHours(0, 0, 0, 0);
+    return sundayDate.getTime() === sundayBeforeEaster.getTime();
+  }
+
+  isSundayBeforeChristmas(sundayDate) {
+    sundayDate = new Date(sundayDate);
+    sundayDate.setHours(0, 0, 0, 0);
+    if (sundayDate.getMonth() !== 11) return false;
+    const christmas = new Date(sundayDate.getFullYear(), 11, 24);
+    const lastSunday = new Date(christmas);
+    lastSunday.setDate(christmas.getDate() - christmas.getDay());
+
+    for (let i = 0; i < 3; i++) {
+      const candidate = new Date(christmas);
+      candidate.setDate(lastSunday.getDate() - i * 7);
+      candidate.setHours(0, 0, 0, 0);
+      if (candidate.getTime() === sundayDate.getTime()) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 
--- src/embeddedjs/main.js
+++ src/embeddedjs/main.js
@@ -15,11 +15,12 @@ const white = render.makeColor(255, 255, 255);
 
 
 
-function draw(event) {
-    const now = event.date;
-    const nextSunday = dateUtils.getNextSundayDate()
+function draw() {
+    const now = new Date();
+    const nextSunday = dateUtils.getNextSundayDate();
+    const prefix = now.getMonth() === nextSunday.getMonth() && now.getDate() === nextSunday.getDate() ? 'Dzisiejsza' : "Następna";
     const mainText = dateUtils.isNextSundayNormal() ? "Normalna" : "PoPiErdOLOnA";
-    const title = `Następna niedziela (${nextSunday.getDate().toString().padStart(2, "0")}.${(nextSunday.getMonth() + 1).toString().padStart(2,"0")}) jest:`;
+    const title = `${prefix} niedziela (${nextSunday.getDate().toString().padStart(2, "0")}.${(nextSunday.getMonth() + 1).toString().padStart(2,"0")}) jest:`;
 
     render.begin();
     render.fillRectangle(black, 0, 0, render.width, render.height);
@@ -30,31 +31,18 @@ function draw(event) {
     render.drawText(title, smallTitleFont, white, (render.width - smallTitleWidth) / 2, (render.height / 2) - smallTitleFont.height - mainFont.height - 10);
     
     render.end();
-}
+};
 
-watch.addEventListener("minutechange", (e) => {
-    draw(e);
-});
-    // Format time as HH:MM
-//     const hours = String(now.getHours()).padStart(2, "0");
-//     const minutes = String(now.getMinutes()).padStart(2, "0");
-//     const timeStr = `${hours}:${minutes}`;
+draw();
 
-    // Center the time vertically (shifted up slightly to make room for date)
-//     let width = render.getTextWidth(timeStr, timeFont);
-//     render.drawText(timeStr, timeFont, white,
-//         (render.width - width) / 2,
-//         (render.height / 2) - timeFont.height + 5);
+// console.log('last sunday of normal month - ', dateUtils.isNextSundayNormal());
+// console.log('25.01 exact (true)- ', dateUtils.isNextSundayNormal(new Date('2026-01-25')));
+// console.log('23.03 before easter (true) - ', dateUtils.isNextSundayNormal(new Date('2026-03-23')));
+// console.log('05.04 easter (false) - ', dateUtils.isNextSundayNormal(new Date('2026-05-04')));
+// console.log('25.11 (false) - ', dateUtils.isNextSundayNormal(new Date('2026-11-25')));
+// console.log('06.12 (true) - ', dateUtils.isNextSundayNormal(new Date('2026-12-06')));
+// console.log('13.12 (true) - ', dateUtils.isNextSundayNormal(new Date('2026-12-13')));
+// console.log('20.12 (true) - ', dateUtils.isNextSundayNormal(new Date('2026-12-20'))); 
 
-    // Format date as "Mon Jan 01"
-//     const dayName = DAYS[now.getDay()];
-//     const monthName = MONTHS[now.getMonth()];
-//     const dateStr = `${dayName} ${monthName} ${String(now.getDate()).padStart(2, "0")}`;
-
-    // Draw date below the time
-//     width = render.getTextWidth(dateStr, dateFont);
-//     render.drawText(dateStr, dateFont, white,
-//         (render.width - width) / 2,
-//         (render.height / 2) + 10);
-
-//     render.end();
+// console.log('01.04 (false) - ', dateUtils.isNextSundayNormal(new Date('2026-04-01'))); 
+// console.log('07.05 (false) - ', dateUtils.isNextSundayNormal(new Date('2026-07-05')));
